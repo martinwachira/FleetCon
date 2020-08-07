c2f962c4b55d4b47c40967bb47e8f4c778027617
martin-wachira <martinnwachira@gmail.com>
2020-08-07 21:22:14 +0300

    added middleware auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 5%">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="">
+                <h3 class="card-header" style="text-align: center"><strong> {{ __('Fleetcon User Login') }}</strong></h3>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -18,9 +18,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,28 +32,29 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-">
+                                <button type="submit" class="btn btn-success float-right">
+                                   <strong>{{ __('Login here') }}</strong>
+                                </button>
                             </div>
-                        </div>                       
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
+    </div>
+    <hr>
+    <br>
+    <div style="text-align: left; margin-bottom: 20%">
+        <!-- footer -->
+        @include('helpers/footer')
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,125 +21,116 @@ Route::get('admin', function () {
     return view('admin');
 });
 
+Auth::routes();
 
 // ! route to get the first step of adding the truck. 
 
-Route::get('addHorse','TruckController@addHorse');
+Route::get('addHorse', 'TruckController@addHorse')->middleware('auth');
 
 // ! route to post horse data. 
 
-Route::post('/postHorseData','TruckController@postHorseData');
+Route::post('/postHorseData', 'TruckController@postHorseData')->middleware('auth');
 
 // ! route to post trailer data. 
-Route::post('/postTrailerData','TruckController@postAddTrailerData');
+Route::post('/postTrailerData', 'TruckController@postAddTrailerData')->middleware('auth');
 
 // ! route to get trailer. 
-Route::get('addTrailer','TruckController@gettingAddTrailer');
+Route::get('addTrailer', 'TruckController@gettingAddTrailer')->middleware('auth');
 
 // ! posting financial data. 
 
-Route::post('/postFinancialData','TruckController@postFinancial');
+Route::post('/postFinancialData', 'TruckController@postFinancial')->middleware('auth');
 
 // ! route to get all the trucks. 
 
-Route::resource('trucks', 'TruckController');
+Route::resource('trucks', 'TruckController')->middleware('auth');
 
 // ! route to get the editing of a truck. 
 
-Route::get('/editTruck/{id}','TruckController@gettingEditPage');
+Route::get('/editTruck/{id}', 'TruckController@gettingEditPage')->middleware('auth');
 
 // ! updating truck. 
 
-Route::post('/updateTruck/{id}','TruckController@updateTruck');
-
-// ! edit driver
-// Route::put('/updateDriver', 'DriverController@update');
-
-// ! edit site
-// Route::put('/updateSite', 'SiteController@update');
+Route::post('/updateTruck/{id}', 'TruckController@updateTruck')->middleware('auth');
 
 // ! route to delete truck. 
-Route::post('deleteTruck','TruckController@destroy');
+Route::post('deleteTruck', 'TruckController@destroy')->middleware('auth');
 
 
 // ! route to delete a driver
-Route::post('deleteDriver','DriverController@destroy');
+Route::post('deleteDriver', 'DriverController@destroy')->middleware('auth');
 
 // ! route to delete a site
-Route::post('deleteSite','SiteController@destroy');
+Route::post('deleteSite', 'SiteController@destroy')->middleware('auth');
 
 // ! route to delete a site
-Route::post('deleteRoute','RouteController@destroy');
+Route::post('deleteRoute', 'RouteController@destroy')->middleware('auth');
 
 Route::get('/main', function () {
-    return view('dashboard/main');
+    return view('dashboard/main')->middleware('auth');
 });
 
 Route::get('/add-driver', function () {
-    return view('driver/create');
+    return view('driver/create')->middleware('auth');
 });
 
 Route::get('drivers', function () {
-    return view('driver/view');
+    return view('driver/view')->middleware('auth');
 });
 
 // ! route to get the form to add a route. 
 
-Route::get('/add-route','RouteController@addRoute');
+Route::get('/add-route', 'RouteController@addRoute')->middleware('auth');
 
 Route::get('routes', function () {
-    return view('route/view');
+    return view('route/view')->middleware('auth');
 });
 
 Route::get('/add-site', function () {
-    return view('site/create');
+    return view('site/create')->middleware('auth');
 });
 
 Route::get('sites', function () {
-    return view('site/view');
+    return view('site/view')->middleware('auth');
 });
 
-Auth::routes();
-
 // Routes here
-Route::resource('/drivers', 'DriverController');
-Route::post('add-driver', 'DriverController@store');
+Route::resource('/drivers', 'DriverController')->middleware('auth');
+Route::post('add-driver', 'DriverController@store')->middleware('auth');
 
-Route::resource('/routes', 'RouteController');
-Route::post('add-route', 'RouteController@store');
-Route::get('getSites', 'RouteController@getSites');
+Route::resource('/routes', 'RouteController')->middleware('auth');
+Route::post('add-route', 'RouteController@store')->middleware('auth');
+Route::get('getSites', 'RouteController@getSites')->middleware('auth');
 
 
-Route::resource('/sites', 'SiteController');
-Route::post('add-site', 'SiteController@store');
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/sites', 'SiteController')->middleware('auth');
+Route::post('add-site', 'SiteController@store')->middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 // ! Routes to the Reports. 
 
 Route::get('driver_performance', function () {
-    return view('Reports/driver_perfomance');
+    return view('Reports/driver_perfomance')->middleware('auth');
 });
 
 Route::get('fuel_usage', function () {
-    return view('Reports/fuel_usage');
+    return view('Reports/fuel_usage')->middleware('auth');
 });
 
 Route::get('route_profitability', function () {
-    return view('Reports/route_profitability');
+    return view('Reports/route_profitability')->middleware('auth');
 });
 
 Route::get('truck_availability', function () {
-    return view('Reports/truck_availability');
+    return view('Reports/truck_availability')->middleware('auth');
 });
 
 Route::get('truck_revenue', function () {
-    return view('Reports/truck_revenue');
+    return view('Reports/truck_revenue')->middleware('auth');
 });
 Route::get('route_revenue', function () {
-    return view('Reports/route_revenue');
+    return view('Reports/route_revenue')->middleware('auth');
 });
